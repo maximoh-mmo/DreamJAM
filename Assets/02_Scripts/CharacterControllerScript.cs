5d1a5deb3d605650261f0948c5423ea88ee8accd
Max Heinze <maxheinze@googlemail.com>
2023-11-03 21:46:52 +0100

    chr controller upgraded

--- Assets/02_Scripts/CharacterControllerScript.cs
+++ Assets/02_Scripts/CharacterControllerScript.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    float _speed = 1f;
+    [SerializeField] float _speedMultiplier = 1f;
+    float _horizontal = 0f;
+    bool _jumpRequested = false;
+    bool _isJumping = false;
+    int _gravity = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_gravity!=0) { ApplyGravity(); }
+        if (Input.GetKeyDown(KeyCode.Space)) { _jumpRequested = true; }
+        _horizontal = Input.GetAxis("Horizontal");
+        if (_horizontal != 0) { MoveCharacter(); }
+        if (_jumpRequested) { DoJump(); }
+    }
+    void DoJump()
+    {
+        Debug.Log("I'm Jumping");
+        _jumpRequested = false;
+        _isJumping = true;
+    }
+    void MoveCharacter()
+    {
+        Debug.Log("horizontal = " +  _horizontal);
+    }
+    void ApplyGravity()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y -9.807f* _gravity * Time.deltaTime);
     }
 }
