340828fbc52cb9ab5da566cd99d107d30fe14969
Max Heinze <maxheinze@googlemail.com>
2023-11-04 12:42:03 +0100

    fixed movement, jump and walk, settings to be tested

--- Assets/02_Scripts/CharacterControllerScript.cs
+++ Assets/02_Scripts/CharacterControllerScript.cs
@@ -6,12 +6,14 @@ public class CharacterController : MonoBehaviour
     [SerializeField] float _jumpModifier = 1f;
     [SerializeField] float _speedMultiplier = 1f;
     float _horizontal = 0f;
-    bool _jumpRequested = false;
-    bool _isJumping = false;
-    int _gravity = 0;
+    [SerializeField] bool _jumpRequested = false;
+    [SerializeField] bool _isJumping = false;
+    [SerializeField] int _gravity = 1;
     Rigidbody2D rb;
     BoxCollider2D coll;
     [SerializeField]LayerMask _ground;
+    float _gravityScale = 1f;
+    float _fallingGravityScale = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,30 +24,48 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // (_gravity!=0) { ApplyGravity(); }
-        if (Input.GetKeyDown(KeyCode.Space)) { _jumpRequested = true; }
+        if (_gravity != 0) { ApplyGravity(); }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) { _jumpRequested = true; }
         _horizontal = Input.GetAxis("Horizontal");
         if (_horizontal != 0) { MoveCharacter(); }
         if (_jumpRequested && isGrounded()) { DoJump(); }
+
     }
     void DoJump()
     {
-        Debug.Log("I'm Jumping");
-        rb.AddForce(Vector2.up * _gravity * _jumpModifier, ForceMode2D.Impulse);
-        _jumpRequested = false;
+        if (_gravity!=0)
+        {
+            rb.AddForce(Vector2.up * _gravity * _jumpModifier, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.AddForce(Vector2.up * _jumpModifier, ForceMode2D.Impulse);
+        }
         _isJumping = true;
-        
+        _jumpRequested = false;
     }
     void MoveCharacter()
     {
         rb.velocity = new Vector2(_horizontal * _speedMultiplier, rb.velocity.y);
     }
     void ApplyGravity()
-    { 
-        rb.velocity = Vector2.up * -9.807f * _gravity * Time.deltaTime;
+    {
+        if (rb.velocity.y >= 0 && _isJumping)
+        {
+            rb.gravityScale = _gravityScale * _gravity;
+        }
+        else if (rb.velocity.y < 0 && _isJumping)
+        {
+            rb.gravityScale = _fallingGravityScale * _gravity;
+        }
     }
     private bool isGrounded()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, _ground);
+        if (Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, _ground))
+        {
+            _isJumping = false;
+            return true;
+        }
+        return false;
     }
 }
