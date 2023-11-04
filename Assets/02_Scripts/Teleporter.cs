4aa91425654eb49562cbeac184d9cad22b0f8427
maximoh <58399612+maximoh-mmo@users.noreply.github.com>
2023-11-04 22:01:24 +0100

    TELEPORTATION

---
+++ Assets/02_Scripts/Teleporter.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]GameObject _PortalExit;
+    GameObject _collided;
+    bool isReady = true;
+    float cooldown = 1f;
+    public bool IsReady { set { isReady = value; } }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        Debug.Log(coll.gameObject.name);
+        _collided = coll.gameObject;
+        if (_collided.tag == "Player")
+        {
+            Debug.Log("player Detected!!!");
+            if (isReady) {
+                doCooldown();
+                _PortalExit.GetComponent<Teleporter>().IsReady = false;
+                _PortalExit.GetComponent<Teleporter>().doCooldown();
+                coll.transform.position = new Vector3(_PortalExit.transform.position.x, _PortalExit.transform.position.y, 0);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+    public void doCooldown()
+    {
+        StartCoroutine(Cooldown());
+    }
+    IEnumerator Cooldown()
+    {
+        Debug.Log("Teleported");
+        yield return new WaitForSeconds(cooldown);
+        isReady = true;
+        Debug.Log("IsReady = " + isReady);
+    }
+}
