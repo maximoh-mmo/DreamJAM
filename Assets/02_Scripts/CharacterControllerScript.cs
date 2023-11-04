90a18e5274024c55f3edb183d52833f875257220
Max Heinze <maxheinze@googlemail.com>
2023-11-04 11:38:30 +0100

    ground/tilemap tests

--- Assets/02_Scripts/CharacterControllerScript.cs
+++ Assets/02_Scripts/CharacterControllerScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
@@ -10,9 +8,14 @@ public class CharacterController : MonoBehaviour
     bool _jumpRequested = false;
     bool _isJumping = false;
     int _gravity = 0;
+    Rigidbody2D rb;
+    BoxCollider2D coll;
+    LayerMask _ground;
     // Start is called before the first frame update
     void Start()
     {
+        coll = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,20 +25,27 @@ public class CharacterController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) { _jumpRequested = true; }
         _horizontal = Input.GetAxis("Horizontal");
         if (_horizontal != 0) { MoveCharacter(); }
-        if (_jumpRequested) { DoJump(); }
+        if (_jumpRequested && isGrounded()) { DoJump(); }
     }
     void DoJump()
     {
         Debug.Log("I'm Jumping");
+
         _jumpRequested = false;
         _isJumping = true;
+        
     }
     void MoveCharacter()
     {
-        transform.position = new Vector2(transform.position.x + _horizontal * Time.deltaTime,transform.position.y);
+        rb.velocity = new Vector2(_horizontal * _speedMultiplier, rb.velocity.y);
+        //        transform.position = new Vector2(transform.position.x + _horizontal * Time.deltaTime,transform.position.y);
     }
     void ApplyGravity()
     {
         transform.position = new Vector2(transform.position.x, transform.position.y -9.807f* _gravity * Time.deltaTime);
+    }
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, _ground);
     }
 }
