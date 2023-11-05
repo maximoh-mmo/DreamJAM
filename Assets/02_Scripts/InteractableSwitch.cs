2c565357dab5a4a39f629ce30bbd578d5c45e46c
maximoh <58399612+maximoh-mmo@users.noreply.github.com>
2023-11-05 13:15:53 +0100

    lightswitch working

--- Assets/02_Scripts/InteractableSwitch.cs
+++ Assets/02_Scripts/InteractableSwitch.cs
@@ -1,11 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractableSwitch : MonoBehaviour
 {
     [SerializeField] float cooldown = 1f;
+    GameObject toDisable=null;
     bool isReady = true;
     public void Trigger()
     {
@@ -14,10 +13,26 @@ public class InteractableSwitch : MonoBehaviour
             StartCoroutine(Cooldown());
         }
     }
+
+    public void doAthingDependingOnMyName()
+    {
+        Debug.Log("My name is> "+this.gameObject.name);
+        if (this.gameObject.name == "LightSwitch") {
+            if (toDisable == null) { toDisable = GameObject.Find("DisableDuringDay"); }
+            if (toDisable.activeSelf == true)
+            {
+                toDisable.SetActive(false);
+            }
+            else
+            {
+                toDisable.SetActive(true);
+            }
+        }
+    }
     IEnumerator Cooldown() 
     { 
         isReady = false;
-        Debug.Log("Triggered");
+        doAthingDependingOnMyName();
         yield return new WaitForSeconds(cooldown);
         isReady = true;
         Debug.Log("Ready to be retriggered");
