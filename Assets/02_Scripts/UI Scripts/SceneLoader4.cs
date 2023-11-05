28d553334ffd286d06f659815bac5487cdb70cfb
M00ncake96 <chantelleschmitz96@gmail.com>
2023-11-05 12:14:43 +0100

    Added main menu UI

---
+++ Assets/02_Scripts/UI Scripts/SceneLoader4.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader4 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Nightmare_Level");
+    }
+}
